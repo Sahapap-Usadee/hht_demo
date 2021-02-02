2b88c5e4652d12343e96ddc6f720b7356fdf2796
Sahapap Usadee <us.sahapap@gmail.com>
2021-02-02 15:43:36 +0700

    commit

--- app/src/main/java/com/jastec/hht_demo/remote/APIUtils.java
+++ app/src/main/java/com/jastec/hht_demo/remote/APIUtils.java
@@ -3,7 +3,7 @@ package com.jastec.hht_demo.remote;
 public class APIUtils {
     private APIUtils() {
     }
-   // http://192.168.1.40:5100  , http://10.0.2.2:5000/
+   // http://192.168.1.40:5100  , http://10.0.2.2:5000/ "http://192.168.1.70:5100/"
     public static final String BASE_URL = "http://192.168.1.70:5100/";
 
 
